3d7758c7ffce3699aa7c3bfa22c44fd132c96761
Kat Wilson <Kat.Wilson@wmg.com>
2022-03-04 16:38:51 +0000

    edits

---
+++ data_check.R
@@ -0,0 +1,19 @@
+## check the datasets
+
+
+month <- read_tsv('/cloud/project/raw/monthly_ghosttown.tsv') %>%
+  filter(COUNTRY_CODE == "US") %>%
+  filter(MAJOR_GENRE_DESC == "Pop")
+weekly <- read_tsv('/cloud/project/raw/weekly_ghosttown.tsv') %>%
+  filter(COUNTRY_CODE == "US") %>%
+  filter(MAJOR_GENRE_DESC == "Pop")
+## caclculate weekly in October
+1468174 + 1716560
+daily <- read_tsv('/cloud/project/raw/daily_ghosttown.tsv')  %>%
+  filter(COUNTRY_CODE == "US") %>%
+  filter(MAJOR_GENRE_DESC == "Pop")
+
+## calculate daily in October
+separate(daily, "DATE_KEY", c("Year", "Month", "Day"), sep = "-") %>%
+  filter(Month == '03') %>%
+  summarise(n = sum(as.numeric(TOTAL_STREAMS)))
--- script_1.R
+++ script_1.R
@@ -4,11 +4,13 @@
 # in monthly, weekly, daily. Issue with ts() not matching ggplot
 # output. Plots only for US. Next step -> compare by countries
 
-
+## STEP 0: libraries
 ### library
 library(dplyr)
 library(hrbrthemes)
 
+
+## Step 1: Monthly Data
 #######
 ###monthly
 #######
@@ -16,13 +18,11 @@ library(tidyverse)
 df <- read_tsv('/cloud/project/raw/monthly_ghosttown.tsv')
 ### monthly total streams of ghost town in 5 year period
 small <- df %>%
-  filter(COUNTRY_CODE == "US")
+  filter(COUNTRY_CODE == "US") %>%
+  filter(MAJOR_GENRE_DESC == "Pop")
 small_sep <- separate(small, "DATE_KEY", c("Year", "Month", "Day"), sep = "-")
-# ts way 1
-pass.ts <- ts(TOTAL_STREAMS, start = c(2021,10), freq = 12)
-plot(pass.ts, ylab = "streams", main = "monthly streams")
-# ts way 2
-streams.ts <- ts(small[,6], start =c(2021,1), freq = 12)
+# ts way 2 incorrect
+streams.ts <- ts(small[,6], start = c(2021,1,1), freq = 5)
 plot(streams.ts)
 ## ggplot way
 p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS)) +
@@ -33,14 +33,14 @@ p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS)) +
   theme(axis.text.x=element_text(angle=60, hjust=1))
 p
 
-
+## STEP 2: Weekly Data
 #######
 ###weekly
 #######
 df <- read_tsv('/cloud/project/raw/weekly_ghosttown.tsv')
 small <- df %>%
   filter(COUNTRY_CODE == "US")
-# small <- separate(small, "DATE_KEY", c("Year", "Month", "Day"), sep = "-")
+small_sep <- separate(small, "DATE_KEY", c("Year", "Month", "Day"), sep = "-")
 
 # # build a numeric indicator
 # new<- ddply(small, .(COUNTRY_CODE), mutate, id = order(DATE_KEY)) %>%
@@ -60,12 +60,28 @@ plot(stream.ts)
 p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS)) +
   geom_line() + 
   xlab("") +
-  scale_x_date(date_labels = "%Y %w %d") +
+  scale_x_date(date_labels = "%Y-%m-%d") +
   labs(title = "Weekly Streams Ghost Town") +
   theme(axis.text.x=element_text(angle=60, hjust=1))
 p
+### weekly breaks
+p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS)) +
+  geom_line() + 
+  xlab("") +
+  labs(title = "Weekly Streams Ghost Town") +
+  scale_x_date(date_labels = "%m-%d-%y",
+                     date_breaks = "week")
+p
+### monthly breaks
+p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS)) +
+  geom_line() + 
+  xlab("") +
+  labs(title = "Weekly Streams Ghost Town") +
+  scale_x_date(date_labels = "%m-%d-%y",
+               date_breaks = "month")
+p
 
-
+## STEP 3: Daily Data
 #######
 ###daily
 #######
@@ -76,17 +92,40 @@ small <- df %>%
 # small <- separate(small, "DATE_KEY", c("Year", "Month", "Day"), sep = "-")
 # ts way 1
 attach(small)
-pass.ts <- ts(TOTAL_STREAMS, start = c(2021,10), freq = 12)
-plot(pass.ts, ylab = "streams", main = "daily streams")
+stream.ts <- ts(TOTAL_STREAMS , start = c(2021, as.numeric(format(small$DATE_KEY[1], "%j"))),
+                frequency = 365)
+plot(stream.ts, ylab = "streams", main = "daily streams")
 ## ts way 2
-stream.ts <- ts(small[8], start = c(2021,1,1), freq = 5)
+stream.ts <- ts(small[6], start = c(2021, as.numeric(format(small$DATE_KEY[1], "%j"))),
+                frequency = 365)
 plot(stream.ts)
 ## ggplot way
 p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS)) +
   geom_line() + 
   xlab("") +
   labs(title = "Daily Streams Ghost Town") +
+  scale_x_date(date_labels = "%m-%d-%y",
+               date_breaks = "week")+
   theme(axis.text.x=element_text(angle=60, hjust=1))
+p
+
+
+### STEP 4: trends by country
+df <- read_tsv('/cloud/project/raw/weekly_ghosttown.tsv')
+small <- df %>%
+  filter(COUNTRY_CODE %in% c("US", "GB", "RS")) %>%
+  filter(MAJOR_GENRE_DESC == "Pop")
+small_sep <- separate(small, "DATE_KEY", c("Year", "Month", "Day"), sep = "-")
+pass.ts <- ts(small[6])
+plot(pass.ts, ylab = "streams", main = "weekly streams", xlim = c(0,20))
+## ggplot way
+p <- ggplot(small, aes(x=DATE_KEY, y=TOTAL_STREAMS,
+                       col = COUNTRY_CODE)) +
+  geom_line() + 
+  xlab("") +
+  scale_x_date(date_labels = "%Y-%m-%d") +
+  labs(title = "Weekly Streams Ghost Town") +
+  theme(axis.text.x=element_text(angle=60, hjust=1)) 
 p
 
 
